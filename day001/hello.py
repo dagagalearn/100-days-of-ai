e87faa957ecf90385e7b90f99c4de5a8d36625a7
dagagalearn <dagagaaddisulearn@gmail.com>
2026-02-15 10:26:50 -0800

    Made the first project for first day
    This script greets the user and calculates their birth year based on their age.

---
+++ day001/hello.py
@@ -0,0 +1,13 @@
+name = input("> What is your name? ")
+print("Hello,", name)
+age = int(input("> How old is "+name+"? "))
+print("Hmm... so you are born in "+str((2026-age)) + " or " + str((2025-age))+ "!")
+
+"""
+Example Output 
+> What is your name? Dave
+Hello, Dave
+> How old is Dave? 12
+Hmm... so you are born in 2014 or 2013!
+
+"""
