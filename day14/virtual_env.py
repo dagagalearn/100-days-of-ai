c405711ab119423c297b321eb1baa221ddf149c9
dagagalearn <dagagaaddisulearn@gmail.com>
2026-02-28 20:59:19 +0300

    Create virtual_env.py for Flask setup instructions
    Add instructions for setting up a Python virtual environment with Flask.

---
+++ day14/virtual_env.py
@@ -0,0 +1,20 @@
+"""
+mkdir project_13
+cd project_13
+
+py -m venv venv
+venv\Scripts\activate
+
+py -m pip install flask==3.0.2
+
+py -m pip freeze > requirements.txt
+
+deactivate
+rmdir /s /q venv   (Windows)
+
+py -m venv venv
+venv\Scripts\activate
+
+py -m pip install -r requirements.txt
+
+"""
